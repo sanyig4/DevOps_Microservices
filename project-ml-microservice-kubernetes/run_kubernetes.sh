10fda9bcfc4e59a67e831a2193bc204fdc38bda0
Sandor Gulyas <57759007+sanyig4@users.noreply.github.com>
2020-04-24 16:51:54 +0200

    Kube configured, CircleCi configured

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="sanyig4/api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run predictionapp\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=predictionapp
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward predictionapp 8000:80
